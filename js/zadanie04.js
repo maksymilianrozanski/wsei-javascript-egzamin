4c2adc33fbe4471b5ec6e4b5a82572a41bbc9d1b
Max <maksymilianrozanski@gmail.com>
2019-12-03 17:02:24 +0100

    solve 4_2

--- js/zadanie04.js
+++ js/zadanie04.js
@@ -2,12 +2,23 @@ window.addEventListener("DOMContentLoaded", () => {
 
     //Zad 4_1
     let simpleClassElements = document.getElementsByClassName("sample_class");
+
     function getTagsOfElements(elements) {
         return Array.from(elements).reduce((tags, current) => {
                 tags.push(current.tagName);
                 return tags
             }
             , [])
+    }
+
+    //Zad 4_2
+    let sampleIdElement = document.getElementById("sample_id");
+
+    function getClassesOfElement(element) {
+        return Array.from(element.classList).reduce((classes, current) => {
+            classes.push(current);
+            return classes
+        }, [])
     }
 });
 
